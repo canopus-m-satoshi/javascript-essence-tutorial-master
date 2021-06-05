80625511bc1c32ecee8250384d3d190b8a762360
Satoshi Matsushima <>
2021-06-05 15:06:09 +0900

    learning about bind method

--- 050_関数とオブジェクト１/070_bindとthis/start/main.js
+++ 050_関数とオブジェクト１/070_bindとthis/start/main.js
@@ -1,15 +1,18 @@
 window.name = 'John';
 
 const person = {
-    name: 'Tom',
-    hello: function() {
-        console.log('Hello ' + this.name);
-    }
-}
-person.hello();
+  name: 'Tom',
+  hello: function () {
+    console.log('Hello ' + this.name);
+  },
+};
+// person.hello();
+
+const helloTom = person.hello.bind(person);
+// const helloTom = person.hello;
 
 function fn(ref) {
-    ref();
+  ref();
 }
 
-fn(person.hello);
+fn(helloTom);
